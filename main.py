ff912bc8414689d285785558e03d58df5a0fcedb
Hugo Guerrero <hugoguerrero03@gmail.com>
2022-08-09 15:55:54 -0500

    Delete comments

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ import schema
 
 model.Base.metadata.create_all(bind=engine)
 
-app = FastAPI(title='API UrlSmartLink', description='API of urlsmartlink', version='0.1')
-print("reiner", app)
+app = FastAPI(title='API UrlSmartLink',
+              description='API of urlsmartlink', version='0.1')
 
 def get_db():
     try:
@@ -19,27 +19,55 @@ def get_db():
     finally:
         db.close()
 
+
 @app.on_event('startup')
 async def startup():
     print('Connecting...')
+
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
+
 @app.get("/status")
 def status():
     return 'Health - Ok'
 
+
 @app.get('/apps/', response_model=List[schema.Apps])
-def show_apps(db:Session=Depends(get_db)):
+def show_apps(db: Session = Depends(get_db)):
     apps = db.query(model.Apps).all()
     return apps
 
-@app.post('/apps/', response_model=schema.Apps)
-def create_apps(insert:schema.Apps,db:Session=Depends(get_db)):
-    apps = model.Apps(id_usuario = insert.id_usuario, nombre=insert.nombre)
+
+@app.post('/apps/', response_model=schema.Apps, summary="Guarda Id_usuario, nombre, fecha  en la tabla Apps")
+def create_apps(insert: schema.Apps, db: Session = Depends(get_db)):
+    apps = model.Apps(id_usuario=insert.id_usuario, nombre=insert.nombre)
     db.add(apps)
     db.commit()
     db.refresh(apps)
     return apps
+
+
+@app.get('/apis/', response_model=List[schema.Apis])
+def show_apis(db: Session = Depends(get_db)):
+    apis = db.query(model.Apis).all()
+    return apis
+
+
+@app.post('/apis/', response_model=schema.Apis, summary="Guarda id_apps, id_usuario, id_estatus, ruta, descripcion, fecha  en la tabla Apps")
+async def read_item(id_apps: int, id_usuario: int, id_status: int, ruta: str, descripcion: str = None):
+    """
+        Detalles de los campos
+
+        - **id**: Es autoincrement.
+        - **id_apps**: Es la relación con la tabla Apps.
+    """
+def create_apis(insert: schema.Apis, db: Session = Depends(get_db)):
+    apis = model.Apis(id_apps=insert.id_apps, id_usuario=insert.id_usuario,
+                      id_status=insert.id_status, ruta=insert.ruta, descripcion=insert.descripcion)
+    db.add(apis)
+    db.commit()
+    db.refresh(apis)
+    return apis
